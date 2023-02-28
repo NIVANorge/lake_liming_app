71f59154df31cbefbea8633d69b93eb2b6063790
nmarquesin <nmarquesin@live.no>
2023-02-28 15:06:56 +0100

    edit title of x axis

--- src/lake_modelling/utils/plot.py
+++ src/lake_modelling/utils/plot.py
@@ -4,7 +4,11 @@ import altair as alt
 
 def plot_result(result, x_title, y_title):
     st.markdown("## Result")
-    chart = alt.Chart(result).mark_line().encode(x=x_title, y=y_title)
+    chart = (
+        alt.Chart(result)
+        .mark_line()
+        .encode(x=alt.X(x_title, axis=alt.Axis(title="Months")), y=y_title)
+    )
     st.altair_chart(chart, use_container_width=True)
 
     return None
